e031b69e8105cb071214827f8b7490cd54c0559d
Aitor P <aitorp@gmail.com>
2019-04-08 16:01:02 +0200

    Cambio en B_eval.m porque no era correcta para materiales no isotropos.

--- lib/eval_B.m
+++ lib/eval_B.m
@@ -5,11 +5,17 @@ Q=(element_jacobian(jem,node_submatrix))\shape_jacobian(jem);
 
 Z=zeros(1,8);
 
+%B=[	Q(1,:),	Z,	Z;
+%	Z,	Q(2,:),	Z;
+%	Z,	Z,	Q(3,:);
+%	Q(2,:),	Q(1,:),	Z;
+%	Z,	Q(3,:),	Q(2,:);
+%	Q(3,:),	Z,	Q(1,:)];
+
 B=[	Q(1,:),	Z,	Z;
 	Z,	Q(2,:),	Z;
 	Z,	Z,	Q(3,:);
-	Q(2,:),	Q(1,:),	Z;
 	Z,	Q(3,:),	Q(2,:);
-	Q(3,:),	Z,	Q(1,:)];
-	
-
+	Q(3,:),	Z,	Q(1,:);
+	Q(2,:),	Q(1,:),	Z;
+    ];
--- lib/eval_B_wedge.m
+++ lib/eval_B_wedge.m
@@ -4,11 +4,17 @@ Q=(element_jacobian_wedge(jem,node_submatrix))\shape_jacobian_wedge(jem);
 
 Z=zeros(1,6);
 
+%B=[	Q(1,:),	Z,	Z;
+%	Z,	Q(2,:),	Z;
+%	Z,	Z,	Q(3,:);
+%	Q(2,:),	Q(1,:),	Z;
+%	Z,	Q(3,:),	Q(2,:);
+%	Q(3,:),	Z,	Q(1,:)];
+
 B=[	Q(1,:),	Z,	Z;
 	Z,	Q(2,:),	Z;
 	Z,	Z,	Q(3,:);
-	Q(2,:),	Q(1,:),	Z;
 	Z,	Q(3,:),	Q(2,:);
-	Q(3,:),	Z,	Q(1,:)];
-	
-
+	Q(3,:),	Z,	Q(1,:);
+	Q(2,:),	Q(1,:),	Z;
+    ];
